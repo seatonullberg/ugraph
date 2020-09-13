f8274782846e5b89ca53de2eeb0103a0673b1a77
Seaton Ullberg <seatonullberg@gmail.com>
2020-09-13 11:08:49 -0400

    Revert back to using void* as generic data. C macros cannot use the type of an argument as a symbol. This prevents the initialization of generic vectors at the same time as the generic vertex structs which contain them.

--- src/ugraph.c
+++ src/ugraph.c
@@ -1,51 +1,51 @@
 // ugraph - C89 undirected graph implementation
 // Copyright (C) Seaton Ullberg and contributors -- MIT license
 
-#include <stddef.h> // size_t
+#include <stddef.h> /* size_t */
 
 #include "cvector.h"
 #include "ugraph.h"
 
-// ugraph_t* ugraph_new() {
-//     cvector_vector_type(ugraph_vertex_t*) v = NULL;
-//     ugraph_t* graph = malloc(sizeof(ugraph_t));
-//     graph->vertices = v;
-//     return graph;
-// }
+ugraph_t* ugraph_create() {
+    cvector_vector_type(ugraph_vertex_t*) v = NULL;
+    ugraph_t* graph = malloc(sizeof(ugraph_t));
+    graph->vertices = v;
+    return graph;
+}
 
-// void ugraph_add_vertex(ugraph_t* graph, void* data) {
-//     ugraph_vertex_t* vertex = malloc(sizeof(ugraph_vertex_t));
-//     vertex->data = data;
-//     cvector_vector_type(ugraph_edge_t*) e = NULL;
-//     vertex->edges = e;
-//     cvector_push_back(graph->vertices, vertex);
-// }
+void ugraph_add_vertex(ugraph_t* graph, void* data) {
+    ugraph_vertex_t* vertex = malloc(sizeof(ugraph_vertex_t));
+    vertex->data = data;
+    cvector_vector_type(ugraph_edge_t*) e = NULL;
+    vertex->edges = e;
+    cvector_push_back(graph->vertices, vertex);
+}
 
-// void ugraph_add_edge(ugraph_t* graph, size_t u, size_t v, void* data) {
-//     /* Connect u to v. */
-//     ugraph_edge_t* uedge = malloc(sizeof(ugraph_edge_t));
-//     uedge->data = data;
-//     uedge->vindex = v;
-//     /* Connect v to u. */
-//     ugraph_edge_t* vedge = malloc(sizeof(ugraph_edge_t));
-//     vedge->data = data;
-//     vedge->vindex = u;
+size_t ugraph_vcount(ugraph_t* graph) {
+    return cvector_size(graph->vertices);
+}
 
-//     cvector_push_back(graph->vertices[u]->edges, uedge);
-//     cvector_push_back(graph->vertices[v]->edges, vedge);
-// }
+void ugraph_add_edge(ugraph_t* graph, size_t u, size_t v, void* data) {
+    /* Connect u to v. */
+    ugraph_edge_t* uedge = malloc(sizeof(ugraph_edge_t));
+    uedge->data = data;
+    uedge->vindex = v;
+    /* Connect v to u. */
+    ugraph_edge_t* vedge = malloc(sizeof(ugraph_edge_t));
+    vedge->data = data;
+    vedge->vindex = u;
+    /* Push new edges into edge vector. */
+    cvector_push_back(graph->vertices[u]->edges, uedge);
+    cvector_push_back(graph->vertices[v]->edges, vedge);
+}
 
-// size_t ugraph_ecount(ugraph_t* graph) {
-//     size_t ne = 0;
+size_t ugraph_ecount(ugraph_t* graph) {
+    size_t ne = 0;
 
-//     size_t i;
-//     size_t nv = ugraph_vcount(graph);
-//     for (i = 0; i < nv; i++) {
-//         ne += cvector_size(graph->vertices[i]->edges);
-//     }
-//     return ne / 2;
-// }
-
-// size_t ugraph_vcount(ugraph_t* graph) {
-//     return cvector_size(graph->vertices);
-// }
+    size_t i;
+    size_t nv = ugraph_vcount(graph);
+    for (i = 0; i < nv; i++) {
+        ne += cvector_size(graph->vertices[i]->edges);
+    }
+    return ne / 2;
+}
